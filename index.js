19ba96c914dd7233bf1715b120ffc5c5ca47ae91
murer <rudy.murer.pro@gmail.com>
2019-03-13 17:14:58 +0000

    Adding variable to color object

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 exports.decorateConfig = (config) => {
+  var candyAppleColor = '#ffbcd9'; // ff0800
   return Object.assign({}, config, {
-    borderColor: 'red',
-    cursorColor: 'red',
+    
+    borderColor: candyAppleColor,
+    cursorColor: candyAppleColor,
     css: `
       ${config.css || ''}
       .tabs_nav .tabs_list .tab_text {
-        color: red;
+        color: candyAppleColor;
       }
       .tabs_nav .tabs_title {
-        color: red;
+        color: candyAppleColor;
       }
     `
+
   });
 }
+
