b6f2fae893a0e3dfefb7897354597965f54be6c0
murer <rudy.murer.pro@gmail.com>
2019-04-21 16:17:43 +0100

    updated theme

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 exports.decorateConfig = (config) => {
-  var candyAppleColor = '#ffbcd9'; // ff0800
+  var candyAppleColor = '#ff0800'; // ff0800
+  var solarizedDarkColor = '#002b36' // Solarized Dark
+  var cursorShape = 'BEAM' // // The shape of the caret in the terminal. Available options are: 'BEAM', 'UNDERLINE', 'BLOCK'
+  var cursorBlink = false;
   return Object.assign({}, config, {
-    
+    backgroundColor : solarizedDarkColor,
+    cursorShape : cursorShape,
+    cursorBlink : cursorBlink,
+    showHamburgerMenu : false,
     borderColor: candyAppleColor,
     cursorColor: candyAppleColor,
     css: `
